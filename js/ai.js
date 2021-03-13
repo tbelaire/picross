a9a1931e54cbab4e84a11e6e5376c6cc0346aa85
Conrad Belaire <cbelaire@gmail.com>
2021-03-13 15:25:36 -0500

    minSpace func

--- js/ai.js
+++ js/ai.js
@@ -11,6 +11,14 @@ class Ai {
         this.model = model;
         console.log("Loaded with a model");
     }
+    
+    // The minimum space a set of hints can fit into.
+    minSpace(hints) {
+        let num = hints.reduce((a, b) => a + b, 0) + hints.length - 1;
+        return num;
+    }
+    
+    // Executed whenever the AI Step button is pressed
     step() {
       console.log("One small step for ai, one giant leap for AIKIND");
     }
