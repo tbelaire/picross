7153c9fcef7945a2d6aa0a0a0f5fc1e21d9a41b0
Theo Belaire <theo.belaire@gmail.com>
2021-03-20 15:38:34 -0400

    Added types to ai.js

--- src/ai.ts
+++ src/ai.ts
@@ -6,6 +6,13 @@ $(function() {
 function testFunc() {console.log("lmao");}
 testFunc();
 
+enum CellState {
+  UNKNOWN = 0,
+  BLOCK = 1,
+  HIT = 2,
+}
+type Line = Array<CellState>;
+
 class Ai {
     model: any;
 
@@ -15,7 +22,7 @@ class Ai {
     }
     
     // The minimum space a set of hints can fit into.
-    minSpace(hints) {
+    minSpace(hints: Array<number>): number {
         let num = hints.reduce((a, b) => a + b, 0) + hints.length - 1;
         return num;
     }
@@ -25,11 +32,11 @@ class Ai {
       console.log("One small step for ai, one giant leap for AIKIND");
     }
 
-    getRow(i) {
-        return this.model.get('state')[i];
+    getRow(i: number): Line {
+        return this.model.get('state')[i].map(entry => entry.abs())
     }
-    getCol(j) {
-        return this.model.get('state').map(row => row[j]);
+    getCol(j: number): Line {
+        return this.model.get('state').map(row => row[j].abs());
     }
 
     getCell(i, j) {
